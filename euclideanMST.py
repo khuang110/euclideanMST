134aaf4c2c404425319f0ee27cb0b41cbbe0902d
Kyle <huangkyle110@gmail.com>
2021-02-19 11:15:32 -0800

    added graph.txt to read from, and func to process its data

--- euclideanMST.py
+++ euclideanMST.py
@@ -35,7 +35,7 @@ class Graph:
     def __init__(self, _k):
         self._k = _k
         self.sets = []
-        self.g = []*_k
+        self.g = []
 
     def add_vertex(self, x, y):
         self.g.append([x, y])
@@ -43,18 +43,44 @@ class Graph:
 
 # extract data from graph.txt
 def read_file():
-    with 
+    lines = []
+    with open('graph.txt', 'r') as r:
+        lines = [line.rstrip() for line in r]
+    return lines
 
+
+# process data from the files
+def process_lines(lines):
+    itr = iter(lines)
+    # t: number of test cases
+    t = int(next(itr))
+    test_cases = []
+    curr = 0
+    i = 1
+    while i < len(lines) and curr < t:
+        # k: number of vertices
+        k = int(lines[i])
+
+        g = Graph(k)
+        for k in range(i + 1, k + i + 1):
+            v = lines[k].split()
+            # Add vertex x,y points to graph
+            g.add_vertex(v[0], v[1])
+        # Shift i to move to next graph
+        i = k + 1
+        curr += 1
+        test_cases.append(g)
+    return test_cases
 
 
 
 def kruskal():
     s = Set()
-
     i = 0
 
 
 def main():
+    test_cases_ = process_lines(read_file())
 
 
 
