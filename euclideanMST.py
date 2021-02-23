af1c0cfe14e0daaa6209cb3c8f583c960c9d7ed1
Kyle <huangkyle110@gmail.com>
2021-02-23 07:36:57 -0800

    working and outputs correct

--- euclideanMST.py
+++ euclideanMST.py
@@ -1,28 +1,30 @@
 # Kruskals Algorithm implementation
 # ∀ u, v  V, u = (x1,y1), v = (x2,y2)
 # w(u,v) = d(u, v) = 𝑛𝑒𝑎𝑟𝑒𝑠𝑡𝑖𝑛𝑡 √(𝑥1 − 𝑥2)^2 + (𝑦1 − 𝑦2)^2
-import heapq
-import itertools
+import itertools # Used to find all possible sets
 import math
 
 
 # Disjoint set
 # Union - Find
 class Set:
+    # set: disjoint set of vertices
+    # ln: length of set
     def __init__(self, n):
         self.set = [-1]*n
         self.ln = 0
 
-    def union(self, r1, r2):
-
-        if self.set[r1] < self.set[r2]:
-            self.set[r1] = r2
+    # Create a union of two sets
+    def union(self, v1, v2):
+        if self.set[v1] < self.set[v2]:
+            self.set[v1] = v2
         else:
-            if self.set[r1] == self.set[r1]:
-                self.set[r1] -= 1
-            self.set[r2] = r1
+            if self.set[v1] == self.set[v1]:
+                self.set[v1] -= 1
+            self.set[v2] = v1
         self.ln += 1
 
+    # Find value in set
     def find(self, r):
         x = r[0] + r[1]
         if self.set[x] < 0:
@@ -33,6 +35,7 @@ class Set:
         return n
 
 
+# Definition of graph
 class Graph:
     # _k: number of vertices
     # g: graph, adjacency matrix implementation
@@ -42,68 +45,77 @@ class Graph:
         self.sets = {}
         self.g = []
 
+    # Add a vertex to graph
     def add_vertex(self, x, y):
-        heapq.heappush(self.g, [x, y])
-        #self.g.append([x, y])
+        self.g.append([x, y])
 
+    # help sort, return calculation to sort by
     def help_sort(self, s):
-        d = calc_distance(s[0], 1)
+        d = calc_distance(s[0])
         return d
 
+    # Go through graph and find all possible sets
     def find_sets(self):
         idx = 0
         s = {}
+        # Itertool.combinations finds all possible sets
         for i in itertools.combinations(self.g, 2):
-            c = calc_distance(i, idx)
+            c = calc_distance(i)
+            # add sets to a dict.
             p = {idx: [i, c]}
             s.update(p)
             idx += 1
         self.sets = [val for key, val in s.items() if val != -1]
+        # Move sets to sorted list
+        # No need to have it put in a dict first, too lazy to remove it..
         self.sets.sort(key=self.help_sort)
 
-
+    # Kruskal's algorithm implementation
     def kruskal(self):
+        # store already used sets
         vertex_sets = Set(self._k*100)
+        # Store edges that are used in soln
         res = []
         i = 0
-        j = 0
+        # Sore edge weight used in soln
         mst_edges = []
-        # = [k for k in self.sets]
 
+        # Loop till
         while i < len(self.sets) and vertex_sets.ln < self._k-1:
             curr_edge = self.sets[i]
 
-            r1 = vertex_sets.find(curr_edge[0][0])
-            r2 = vertex_sets.find(curr_edge[0][1])
+            v1 = vertex_sets.find(curr_edge[0][0])
+            v2 = vertex_sets.find(curr_edge[0][1])
 
-            if r1 != r2:
+            # check if next vertex can be added to mst
+            if v1 != v2:
                 mst_edges.append(curr_edge[1])
                 res.append(curr_edge)
-                vertex_sets.union(r1, r2)
-
+                vertex_sets.union(v1, v2)
             i += 1
         min_cost = 0
+
         print("Edges in MST")
         print("Distance              Point (x, y)")
         for weight in range(0, len(mst_edges)):
             min_cost += mst_edges[weight]
             print_edge(res[weight])
-
         print("Total distance: ", min_cost)
 
+
+# Print out vertex set in mst with distance
 def print_edge(e):
     x1, y1 = e[0][0]
     x2, y2 = e[0][1]
     weight = e[1]
-    print(str(weight)+"                     (" + str(x1) + ", " + str(y1) + ")  -   (" + str(x2) + ", " + str(y2) + ")")
+    print(str(weight)+"                     (" + str(x1)
+          + ", " + str(y1) + ")  -   (" + str(x2) + ", " + str(y2) + ")")
 
 
-
-
-def calc_distance(set_, key):
-    #s1, s2 = set_[key][0]
-    x1, y1 = set_[0]#, s1[1]
-    x2, y2 = set_[1]#, s2[1]
+# Calculate euclidean distance
+def calc_distance(set_):
+    x1, y1 = set_[0]
+    x2, y2 = set_[1]
     distance = math.sqrt((x1-x2)**2+(y1-y2)**2)
     return round(distance)
 
@@ -128,6 +140,7 @@ def process_lines(lines):
         k = int(lines[i])
 
         g = Graph(k)
+        # loop through ordered pairs and put in graph
         for k in range(i + 1, k + i + 1):
             v = lines[k].split()
             # Add vertex x,y points to graph
@@ -139,21 +152,13 @@ def process_lines(lines):
     return test_cases
 
 
+# driver
 def main():
     test_cases_ = process_lines(read_file())
-
-    # test_cases_[1].find_sets()
-    # #test_cases_[0].find_edge_distance()
-    # print("___________________")
-    # from pprint import pprint
-    # pprint(test_cases_[1].sets)
-    # print("____________________")
-    # test_cases_[1].kruskal()
 
     for i in range(0, len(test_cases_)):
         print("Test case: ", i+1)
         test_cases_[i].find_sets()
-        #test_cases_[0].find_edge_distance()
         test_cases_[i].kruskal()
         print()
 
